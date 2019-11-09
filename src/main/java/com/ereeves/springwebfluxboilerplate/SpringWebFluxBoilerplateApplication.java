622662c9cf1b1b0d99f19f9304d3f2ecc599fafc
Erick Tenorio <ereeves@eservicios.indracompany.com>
2019-11-09 21:56:19 +0800

    Initial commit

---
+++ src/main/java/com/ereeves/springwebfluxboilerplate/SpringWebFluxBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.ereeves.springwebfluxboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxBoilerplateApplication.class, args);
+	}
+
+}
